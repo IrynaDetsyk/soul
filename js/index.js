ef0aa1de5b4982e169ea478d188fbb2885531594
Iryna Detsyk <isayyepp@gmail.com>
2018-02-11 18:20:43 +0000

    Initial

---
+++ js/index.js
@@ -0,0 +1,22 @@
+// JavaScript File
+$('#submit-message').click(function(ev) {
+    if ($('form')[0].checkValidity()) {
+        ev.preventDefault();
+        $.ajax({
+            url: "https://formspree.io/isayyepp@gmail.com", 
+            method: "POST",
+            data: {
+                Name: $('#name').val(),
+                Tel: $('#tel').val(),
+                Email: $('#email').val(),
+                Message: $('#message').val(),
+            },
+            dataType: "json"
+        }).done(function() {
+            $('form')[0].reset();
+            $('#form-note').html('<p>Thank You!</p>')
+        }).fail(function(){
+            $('#form-note').html('<p>Error! Fill in the form</p>')
+        });
+    }
+});
